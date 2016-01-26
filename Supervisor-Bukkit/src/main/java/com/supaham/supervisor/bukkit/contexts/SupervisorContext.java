ac370e1b8e0e8022c1a837a59510197a8fd7a433
Ali Moghnieh <SupaHam.me@gmail.com>
2016-01-26 22:16:50 +0000

    Add 'perms' argument in SupervisorContext for listing online players' permissions.

--- Supervisor-Bukkit/src/main/java/com/supaham/supervisor/bukkit/contexts/SupervisorContext.java
+++ Supervisor-Bukkit/src/main/java/com/supaham/supervisor/bukkit/contexts/SupervisorContext.java
@@ -1,14 +1,24 @@
 package com.supaham.supervisor.bukkit.contexts;
 
+import com.google.common.collect.ImmutableMap;
+
 import com.supaham.supervisor.bukkit.SupervisorPlugin;
 import com.supaham.supervisor.report.AbstractReportContextEntry;
 import com.supaham.supervisor.report.ReportContext;
 import com.supaham.supervisor.report.ReportContextEntry;
+import com.supaham.supervisor.report.ReportFile;
 import com.supaham.supervisor.report.ReportSpecifications;
 import com.supaham.supervisor.report.ReportSpecifications.ReportLevel;
+import com.supaham.supervisor.report.SimpleReportFile.PlainTextReportFile;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
@@ -34,12 +44,28 @@ public class SupervisorContext extends ReportContext {
 
         @Override
         public void run() {
+            if (getReportSpecifications().hasArgument("perms")) {
+                perms();
+            }
             if (getReportLevel() > ReportLevel.BRIEF) {
-                try {
-                    createPlainTextFile("config.yml", "Supervisor Config").appendFile(new File(supervisorPlugin.getDataFolder(), "config.yml"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                config();
+            }
+        }
+
+        private void perms() {
+            ReportFile file = createFile("permissions", "Online players' permissions");
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                List<ImmutableMap<String, Boolean>> perms = player.getEffectivePermissions().stream()
+                    .map(s -> ImmutableMap.of(s.getPermission(), s.getValue())).collect(Collectors.toList());
+                file.append(player.getUniqueId().toString(), perms);
+            }
+        }
+
+        private void config() {
+            try {
+                createPlainTextFile("config.yml", "Supervisor Config").appendFile(new File(supervisorPlugin.getDataFolder(), "config.yml"));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
